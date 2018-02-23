8e41207935c52c465d6ae20e9fe4732e3344bded
walterdd <dasha-walter@yandex.ru>
2018-02-23 03:15:42 +0300

    baloons update

--- swiftdaddy/domainlookup/forms.py
+++ swiftdaddy/domainlookup/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, default='dwalter', widget = forms.TextInput(attrs = {'class': 'form-control'}))
-    password = forms.CharField(max_length=30, default='swiftdaddy', widget = forms.PasswordInput(attrs = {'class': 'form-control'}))
+    username = forms.CharField(max_length=30, initial='dwalter', widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    password = forms.CharField(max_length=30, initial='swiftdaddy', widget = forms.PasswordInput(attrs = {'class': 'form-control'}))
